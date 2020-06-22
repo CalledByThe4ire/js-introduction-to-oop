b61131002db2bde0367f4dc9b086fb4aee01ee84
Andrey Karamnoff <karamnoff@gmail.com>
2020-06-22 14:53:42 +0300

    add hexlet' solution for 03-encapsulation

--- users.js
+++ users.js
@@ -1,6 +1,9 @@
-// @ts-check
-/* eslint-disable import/prefer-default-export */
-
-// BEGIN (write your solution here)
-
-// END
+export const getMutualFriends = (user1, user2) => {
+  const friends1 = user1.getFriends();
+  const friends2 = user2.getFriends();
+  // the esiest way to do it is using intersection function from lodash
+  const mutualFriends = friends1.filter((friend1) =>
+    friends2.some((friend2) => friend2.id === friend1.id)
+  );
+  return mutualFriends;
+};
