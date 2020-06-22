a22d482730208c2a04f894e8317dbf9a8aa0952c
Andrey Karamnoff <karamnoff@gmail.com>
2020-06-22 15:08:18 +0300

    add solution for 04-this

--- rational.js
+++ rational.js
@@ -1,5 +1,25 @@
 // @ts-check
 
 // BEGIN (write your solution here)
+const make = (numer, denom) => ({
+  numer,
+  denom,
+  getNumer() {
+    return this.numer;
+  },
+  getDenom() {
+    return this.denom;
+  },
+  toString() {
+    return `${numer}/${denom}`;
+  },
+  add(rat) {
+    return make(
+      this.numer * rat.denom + this.denom * rat.numer,
+      this.denom * rat.denom,
+    );
+  },
+});
 
+export default make;
 // END
