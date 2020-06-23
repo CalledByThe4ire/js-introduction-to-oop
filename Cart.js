844fffccda82ee94ae45a4600ce8dd84c284beb2
Andrey Karamnoff <karamnoff@gmail.com>
2020-06-23 12:51:35 +0300

    add solution for 10-class

--- Cart.js
+++ Cart.js
@@ -4,6 +4,27 @@ import _ from 'lodash';
 
 // BEGIN (write your solution here)
 export default class Cart {
-  //
+  constructor() {
+    this.cart = [];
+  }
+
+  addItem(good, count) {
+    this.cart.push({ good, count });
+  }
+
+  getItems() {
+    return this.cart;
+  }
+
+  getCount() {
+    return this.cart.reduce((acc, { count }) => acc + count, 0);
+  }
+
+  getCost() {
+    return this.cart.reduce(
+      (acc, { good: { price }, count }) => acc + price * count,
+      0,
+    );
+  }
 }
 // END
